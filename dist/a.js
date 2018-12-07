c8fb1e4d5048280901ec8ac50791fdbd82048705
zhong <zhongfanglin777@gmail.com>
2018-12-07 12:02:46 +0800

    ss

---
+++ dist/a.js
@@ -0,0 +1,1 @@
+function a(){console.log(111)}function a(){console.log(111)}function a(){console.log(111)}function a(){console.log(111)}
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,43 @@
-var gulp = require('gulp');
+var gulp=require('gulp');
+var uglify=require ('gulp-uglify') ; 
+var del=require('del');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var babel = require('gulp-babel');
+var connect = require('gulp-connect');
+//压缩js
 gulp.task('default', function() {
-// 将你的默认的任务代码放在这
-   gulp.src('-/js/a.js')
-  .pipe(minify())
-  .pipe(gulp.dest('dest'));
+   gulp.src('js/a.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('dist'));
 });
-//gulp.task('hellow1',function(){
-//	console.log('nihao');
-//});
-//gulp.task('hellow2',function(){
-//	console.log('buhao1');
-//});
-//gulp.task('hellow3',function(){
-//	console.log('buhao2');
-//});
-//gulp.task('hellow',['hellow1','hellow2','hellow3'],function(){
-//	console.log('I am fine');
-//})
-//gulp.task('one',function(cb){
-//	cb(err);
-//})
-//gulp.task('two',['one'],function(){
-//	
-//});
-//gulp.task('default',['one','two']);
+//清除dist
+gulp.task('clean',()=>{
+	del(['dist']);
+});
+//合并JS
+gulp.task('concat',function(){
+   gulp.src('a/**/*.js')
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('dist'));
+});
+//改名
+gulp.task('rename',function(){
+   gulp.src('a/a.js')
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest('dist'));
+});
+//es6->es5
+gulp.task('babel',function(){
+   gulp.src('a/a.js')
+  .pipe(babel({presets: ['@babel/env']}))
+  .pipe(gulp.dest('dist'));
+});
+//连接服务器
+gulp.task('connect',function(){
+   connect.server({
+        root: 'dist',
+        port: '7777',
+        livereload: true
+    });
+});
