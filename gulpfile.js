bf924ae881deec59da6263e1dcc69b69c28da7b1
zhong <zhongfanglin777@gmail.com>
2018-12-10 21:09:10 +0800

    shopcar

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,70 @@
-var gulp=require('gulp');
-var uglify=require ('gulp-uglify') ; 
-var del=require('del');
-var concat = require('gulp-concat');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat=require('gulp-concat');
 var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
 var babel = require('gulp-babel');
-var connect = require('gulp-connect');
-//压缩js
-gulp.task('default', function() {
-   gulp.src('js/a.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('dist'));
+var del=require('del');
+var connect= require('gulp-connect'); 
+var watch = require('gulp-watch');
+var revCollector = require('gulp-rev-collector')
+//生成哈希值
+var rev= require('gulp-rev');
+gulp.task('default',['htmlmin','minijs','watch','connect']);
+gulp.task('minijs',function(){
+	gulp.src('js/**/*.js')
+	 //合并js
+	.pipe(concat('all.js'))
+	//转义es6->es5
+	 .pipe(babel({
+            presets: ['@babel/env']
+        }))
+	//压缩js
+	.pipe(uglify())
+	//生成哈希值
+	.pipe(rev())
+	//重命名js
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('dist'))
+	.pipe(rev.manifest())
+	.pipe(gulp.dest('rev/js'))
+	.pipe(connect.reload());
 });
-//清除dist
-gulp.task('clean',()=>{
+gulp.task('concatjs',function(){
+	gulp.src('js/*.js')
+	.pipe(concat('all.js'))
+	.pipe(gulp.dest('dist'))
+	//自动刷新
+	.pipe(connect.reload());
+});
+gulp.task('htmlmin',function(){
+//	gulp.src('index.html')
+	gulp.src(['rev/**/*.json','*.html'])
+//	.pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(revCollector())
+	.pipe(gulp.dest('dist'))
+	.pipe(connect.reload());
+});
+//打开连接
+gulp.task('connect', function() {
+  connect.server({
+  	//根目录
+  	root:'dist',
+  	//端口号
+  	port: '6666',
+  	//热更新
+  	livereload:true
+  });
+});
+//监听事件
+gulp.task('watch',function(){
+	//监听html
+	gulp.watch('all.html',['htmlmin']);
+	//监听js
+	gulp.watch('js/*.js',['htmlmin']);
+});
+//没有node_modules只需cnpm install;
+//删除文件
+gulp.task('clean',function(){
 	del(['dist']);
-});
-//合并JS
-gulp.task('concat',function(){
-   gulp.src('a/**/*.js')
-  .pipe(concat('all.js'))
-  .pipe(gulp.dest('dist'));
-});
-//改名
-gulp.task('rename',function(){
-   gulp.src('a/a.js')
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('dist'));
-});
-//es6->es5
-gulp.task('babel',function(){
-   gulp.src('a/a.js')
-  .pipe(babel({presets: ['@babel/env']}))
-  .pipe(gulp.dest('dist'));
-});
-//连接服务器
-gulp.task('connect',function(){
-   connect.server({
-        root: 'dist',
-        port: '7777',
-        livereload: true
-    });
-});
+})
--- js/shopCar.js
+++ js/shopCar.js
@@ -1,18 +1,88 @@
 var shopCar=(function(){
 	return {
 		init:function(){
-			this.event();
+			this.lis=document.querySelector('.shopList .lis');
+			this.lists=document.querySelector('.com');
+			this.liAll=this.lists.querySelectorAll('li');
+			this.but=this.lists.querySelectorAll('button');
 			this.getData();
+			this.event();
+			this.carData();
 		},
 		event:function(){
 			const _this=this;
+			for(let i=0;i<this.but.length;i++){
+				this.but[i].onclick=function(){
+                 _this.setItem(_this.data[i]);
+                 _this.lis.innerHTML='';
+                 _this.carData();
+                 document.querySelector('.shopList p').style.display='block';
+                 console.log(document.querySelectorAll('.rm'));
+				}
+			}
+			
 		},
 		getData:function(){
 			sendAjax('json/shopcar.json').then(res=>{
 				res=JSON.parse(res);
-				console.log(res);
+				this.data=res;
+				this.insertData(res);
 			})
-		}
+		},
+		insertData:function(data){
+			for(var i=0;i<this.liAll.length;i++){
+				this.liAll[i].querySelector('img').src=data[i].src;
+				this.liAll[i].querySelector('.con').innerHTML=data[i].contents;
+				this.liAll[i].querySelector('.pri').innerHTML=data[i].price;
+			}
+		},
+		setItem:function(data){
+			var shopList=localStorage.getItem('shopList')||'[]';
+			shopList=JSON.parse(shopList);
+			for(var i=0;i<shopList.length;i++){
+					if(shopList[i].name==data.name){
+						shopList[i].num=parseInt(shopList[i].num)+parseInt(data.num);
+						break;
+					    }
+					}
+				if(i==shopList.length){
+				shopList.push(data);
+				}
+			localStorage.shopList=JSON.stringify(shopList);
+		},
+        carData:function(){
+        	var shopList=localStorage.shopList||'[]';
+			shopList=JSON.parse(shopList);
+			for(var i=0;i<shopList.length;i++){
+				var money=parseInt(shopList[i].price)*parseInt(shopList[i].num);
+	            $li=document.createElement('li');
+				$li.innerHTML=`<div class="contentsAll clearfix">
+							<div class="inp"><input type="checkbox" class='checkbox' /></div>
+							<div class="img">
+								<img src="${shopList[i].src}" />
+							</div>
+							<div class="con">${shopList[i].contents}</div>
+							<div class="colors">
+								<p class="co">颜色：${shopList[i].color}</p>
+								<p class="si">尺寸：${shopList[i].size}</p>
+							</div>
+							<div class="price">${shopList[i].price}</div>
+							<div class="num"><input type="number" value='${shopList[i].num}' /></div>
+							<div class="jiage">
+								<span>${money}.00</span>
+								<i>${shopList[i].weight}</i>
+							</div>
+							<div class="move">
+							<p class='rm'>删除</p>
+							<p>移到我的关注</p>
+							<p>加到我的关注</p>
+						</div>
+						</div>
+						<div class="dazhe">预约中，抢购开始时间2018-12-12 00:00</div>
+					`;
+					this.lis.appendChild($li);
+			}
+        }
 	}
 }());
 shopCar.init();
